d67fba517aa79391c55586850763812d1d45ab7d
Luke <117487478+lb1227@users.noreply.github.com>
2025-10-15 03:16:19 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-// If you named the component WebNovelLanding.jsx and put it in /src/pages
-import WebNovelLanding from './pages/WebNovelLanding.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WebNovelLanding />
+    <App />
   </React.StrictMode>
-)
+);
